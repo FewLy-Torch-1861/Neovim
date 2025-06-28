fa1d1424e2c1059c413cdf438a58491020cd0a80
FewLy-Torch-1861 <furytorch.1861@gmail.com>
2025-06-28 18:47:22 +0700

    make it look better

--- lua/options.lua
+++ lua/options.lua
@@ -1,17 +1,38 @@
--- Indentation
-vim.cmd.set("expandtab")
-vim.cmd.set("tabstop=2")
-vim.cmd.set("softtabstop=2")
-vim.cmd.set("shiftwidth=2")
-
--- Line
-vim.cmd.set("number")
-vim.cmd.set("relativenumber")
-vim.cmd.set("cursorline")
-
--- Clipboard
-vim.cmd.set("clipboard+=unnamedplus")
-
-vim.opt.swapfile = false
-
 vim.wo.number = true
+
+local opt = vim.opt
+local o = vim.o
+local g = vim.g
+
+o.laststatus = 3
+o.showmode = false
+
+o.clipboard = "unnamedplus"
+o.cursorline = true
+o.cursorlineopt = "both"
+
+-- Indenting
+o.expandtab = true
+o.shiftwidth = 2
+o.smartindent = true
+o.tabstop = 2
+o.softtabstop = 2
+
+opt.fillchars = { eob = " " }
+o.ignorecase = true
+o.smartcase = true
+o.mouse = "a"
+
+-- Numbers
+o.number = true
+o.relativenumber = true
+o.numberwidth = 2
+o.ruler = false
+
+o.signcolumn = "yes"
+o.splitbelow = true
+o.splitright = true
+o.timeoutlen = 400
+o.undofile = true
+
+opt.swapfile = false
---
+++ lua/plugins/visual-whitespace.lua
@@ -0,0 +1,6 @@
+return {
+  "mcauley-penney/visual-whitespace.nvim",
+  config = function()
+    -- vim.g.visual_whitespace = {}
+  end,
+}
