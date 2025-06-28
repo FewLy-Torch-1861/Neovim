266a04b7cae075b77ac8fee79707260d3f6ebbf2
FewLy-Torch-1861 <furytorch.1861@gmail.com>
2025-06-28 14:01:35 +0700

    add whichkey!

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,6 +7,7 @@ return {
     local builtin = require("telescope.builtin")
 
     vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+    vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Telescope recent files" })
     vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
     vim.keymap.set("n", "<leader>fz", builtin.current_buffer_fuzzy_find, { desc = "Telescope find in current buffer" })
     vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
---
+++ lua/plugins/whichkey.lua
@@ -0,0 +1,14 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    preset = "modern",
+    win = {
+      padding = { 1, 0 }
+    },
+    layout = {
+      width = { min = 20 },
+      spacing = 1,
+    },
+  }
+}
