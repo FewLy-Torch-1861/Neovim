958110f16a9a413d50f6e13dc313a4376bf63e4c
FewLy-Torch-1861 <furytorch.1861@gmail.com>
2025-06-28 17:38:24 +0700

    just format file

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,72 +1,72 @@
 return {
-	{
-		"mason-org/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"mason-org/mason-lspconfig.nvim",
-		dependencies = {
-			"mason-org/mason.nvim",
-			"neovim/nvim-lspconfig",
-		},
-		config = function()
-			require("mason-lspconfig").setup({
-				automatic_enable = true,
-				ensure_installed = {
-					"lua_ls",
-					"ts_ls",
-					"harper_ls",
-					"eslint",
-					"cssls",
-					"html",
-					"jsonls",
-					"pyright",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.ts_ls.setup({})
-			lspconfig.harper_ls.setup({})
+  {
+    "mason-org/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = {
+      "mason-org/mason.nvim",
+      "neovim/nvim-lspconfig",
+    },
+    config = function()
+      require("mason-lspconfig").setup({
+        automatic_enable = true,
+        ensure_installed = {
+          "lua_ls",
+          "ts_ls",
+          "harper_ls",
+          "eslint",
+          "cssls",
+          "html",
+          "jsonls",
+          "pyright",
+        },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.ts_ls.setup({})
+      lspconfig.harper_ls.setup({})
       lspconfig.eslint.setup({})
       lspconfig.cssls.setup({})
       lspconfig.html.setup({})
       lspconfig.jsonls.setup({})
       lspconfig.pyright.setup({})
 
-			vim.diagnostic.config({
-				virtual_text = {
-					prefix = "●",
-					spacing = 4,
-				},
-				signs = true,
-				underline = true,
-				update_in_insert = false,
-				severity_sort = true,
-			})
+      vim.diagnostic.config({
+        virtual_text = {
+          prefix = "●",
+          spacing = 4,
+        },
+        signs = true,
+        underline = true,
+        update_in_insert = false,
+        severity_sort = true,
+      })
 
-			local function opts(desc)
-				return { buffer = bufnr, desc = "LSP " .. desc }
-			end
+      local function opts(desc)
+        return { buffer = bufnr, desc = "LSP " .. desc }
+      end
 
-			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts("Go to declaration"))
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts("Go to definition"))
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts("Go to declaration"))
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts("Go to definition"))
 
-			vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts("Add workspace folder"))
-			vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts("Remove workspace folder"))
-			vim.keymap.set("n", "<leader>wl", function()
-				print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-			end, opts("List workspace folders"))
+      vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts("Add workspace folder"))
+      vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts("Remove workspace folder"))
+      vim.keymap.set("n", "<leader>wl", function()
+        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+      end, opts("List workspace folders"))
 
-			vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts("Go to type definition"))
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, opts("Buffer Hover"))
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts("Code actions"))
-		end,
-	},
+      vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts("Go to type definition"))
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts("Buffer Hover"))
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts("Code actions"))
+    end,
+  },
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,39 +1,39 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependencies = {
-		"nvimtools/none-ls-extras.nvim",
-	},
-	config = function()
-		local null_ls = require("null-ls")
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
+  config = function()
+    local null_ls = require("null-ls")
 
-		null_ls.setup({
-			sources = {
-				-- Lua
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.completion.luasnip,
+    null_ls.setup({
+      sources = {
+        -- Misc
+        null_ls.builtins.completion.spell,
+        null_ls.builtins.formatting.prettier,
 
-				-- Python
-				null_ls.builtins.diagnostics.pylint.with({
-					diagnostics_postprocess = function(diagnostic)
-						diagnostic.code = diagnostic.message_id
-					end,
-				}),
-				null_ls.builtins.formatting.isort,
-				null_ls.builtins.formatting.black,
+        -- Lua
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.completion.luasnip,
 
-				-- Ruby
-				null_ls.builtins.formatting.rubocop,
-				null_ls.builtins.diagnostics.rubocop,
+        -- Python
+        null_ls.builtins.diagnostics.pylint.with({
+          diagnostics_postprocess = function(diagnostic)
+            diagnostic.code = diagnostic.message_id
+          end,
+        }),
+        null_ls.builtins.formatting.isort,
+        null_ls.builtins.formatting.black,
 
-				-- JS/TS
-				null_ls.builtins.diagnostics.eslint_d,
+        -- Ruby
+        null_ls.builtins.formatting.rubocop,
+        null_ls.builtins.diagnostics.rubocop,
 
-				-- Misc
-				null_ls.builtins.completion.spell,
-				null_ls.builtins.formatting.prettier,
-			},
-		})
+        -- JS/TS
+        null_ls.builtins.diagnostics.eslint_d,
+      },
+    })
 
-		vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "Lsp format buffer" })
-	end,
+    vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, { desc = "Lsp format buffer" })
+  end,
 }
