167a6dd27012bae5c8ad7df4fd8995a9a005095b
FewLy-Torch-1861 <furytorch.1861@gmail.com>
2025-06-28 13:24:08 +0700

    move options.lua and mappings.lua to lua/ folder

--- init.lua
+++ init.lua
@@ -1,17 +1,3 @@
--- Indentation
-vim.cmd.set "expandtab"
-vim.cmd.set "tabstop=2"
-vim.cmd.set "softtabstop=2"
-vim.cmd.set "shiftwidth=2"
-
--- Line
-vim.cmd.set "number"
-vim.cmd.set "relativenumber"
-vim.cmd.set "cursorline"
-
--- Clipboard
-vim.cmd.set "clipboard+=unnamedplus"
-
 -- Leaderkey
 vim.g.mapleader= " "
 
---
+++ lua/options.lua
@@ -0,0 +1,13 @@
+-- Indentation
+vim.cmd.set "expandtab"
+vim.cmd.set "tabstop=2"
+vim.cmd.set "softtabstop=2"
+vim.cmd.set "shiftwidth=2"
+
+-- Line
+vim.cmd.set "number"
+vim.cmd.set "relativenumber"
+vim.cmd.set "cursorline"
+
+-- Clipboard
+vim.cmd.set "clipboard+=unnamedplus"
