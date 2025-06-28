70e399a74aa52435a5597da0601c0a3c140c63d3
FewLy-Torch-1861 <furytorch.1861@gmail.com>
2025-06-28 18:53:35 +0700

    make it better

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -13,17 +13,16 @@ return {
     },
     config = function()
       require("mason-lspconfig").setup({
-        automatic_enable = true,
+        automatic_installation = true,
         ensure_installed = {
           "lua_ls",
           "ts_ls",
-          "harper_ls",
           "eslint",
           "cssls",
           "html",
           "jsonls",
           "pyright",
-          "bash_ls"
+          "bashls",
         },
       })
     end,
@@ -33,61 +32,50 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.harper_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.eslint.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.cssls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.jsonls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.pyright.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.bash_ls.setup({
-        capabilities = capabilities,
-      })
+
+      local servers = {
+        "lua_ls",
+        "ts_ls",
+        "eslint",
+        "cssls",
+        "html",
+        "jsonls",
+        "pyright",
+        "bashls",
+      }
+
+      local on_attach = function(client, bufnr)
+        local function opts(desc)
+          return { buffer = bufnr, desc = "LSP " .. desc }
+        end
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts("Go to declaration"))
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts("Go to definition"))
+        vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts("Add workspace folder"))
+        vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts("Remove workspace folder"))
+        vim.keymap.set("n", "<leader>wl", function()
+          print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, opts("List workspace folders"))
+        vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts("Go to type definition"))
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts("Buffer Hover"))
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts("Code actions"))
+      end
+
+      for _, server in ipairs(servers) do
+        lspconfig[server].setup({
+          capabilities = capabilities,
+          on_attach = on_attach,
+          -- Add per-server settings here if needed, e.g.
+          -- settings = (server == "lua_ls") and { ... } or nil
+        })
+      end
 
       vim.diagnostic.config({
-        virtual_text = {
-          prefix = "●",
-          spacing = 4,
-        },
+        virtual_text = { prefix = "●", spacing = 4 },
         signs = true,
         underline = true,
         update_in_insert = false,
         severity_sort = true,
       })
-
-      local function opts(desc)
-        return { buffer = bufnr, desc = "LSP " .. desc }
-      end
-
-      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts("Go to declaration"))
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts("Go to definition"))
-
-      vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts("Add workspace folder"))
-      vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts("Remove workspace folder"))
-      vim.keymap.set("n", "<leader>wl", function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-      end, opts("List workspace folders"))
-
-      vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts("Go to type definition"))
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, opts("Buffer Hover"))
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts("Code actions"))
     end,
   },
 }
